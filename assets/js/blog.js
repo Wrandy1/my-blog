a590f6c9d75f4e6b44cb789b5a63aa25832503fd
Wrandy1 <cjyoung0420@gmail.com>
2024-10-31 13:10:02 -0500

    readme

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,1 +1,32 @@
-var text = $('#DynamicValueAssignedHere').find('input[name="FirstName"]').val();
+const user = document.getElementById('username');
+const title = document.getElementById('title');
+const content = document.getElementById('content');
+const submitButton = document.getElementById('submit');
+
+
+function savePost() {
+
+  const userPost = {
+    user: user.value,
+    title: title.value,
+    content: content.value.trim(),
+  };
+  localStorage.setItem('userpost', JSON.stringify(userPost));
+}
+
+function renderPost() {
+  // Use JSON.parse() to convert text to JavaScript object
+  const newPost = JSON.parse(localStorage.getItem('userpost'));
+  // Check if data is returned, if not exit out of the function
+  if (newPost !== null) {
+    document.getElementById('saved-user').innerHTML = newPost.user;
+    document.getElementById('saved-title').innerHTML = newPost.title;
+    document.getElementById('saved-content').innerHTML = newPost.content;
+  }
+}
+
+submitButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  savePost();
+  renderPost();
+});
---
+++ assets/js/form.js
@@ -0,0 +1,21 @@
+const submitButton = document.getElementById('submit');
+const user = document.getElementById('username');
+const title = document.getElementById('title');
+const content = document.getElementById('content');
+
+
+// function savePost() {
+
+//   const userPost = {
+//     user: user.value,
+//     title: title.value,
+//     content: content.value.trim(),
+//   };
+//   localStorage.setItem('userpost', JSON.stringify(userPost));
+// }
+
+// submitButton.addEventListener('click', function (event) {
+//   event.preventDefault();
+//   savePost();
+//   renderPost();
+// });
